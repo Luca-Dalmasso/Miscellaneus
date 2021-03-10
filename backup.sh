7ea3f5cbc82bdf0d682c275cacf90246e3a5a55b
Luca Dalmasso <luca.talpax@gmail.com>
2021-03-10 18:59:18 +0100

    added new backup script

---
+++ backup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#simple script that compress recursively a directory (and all subs)
+#in another directory
+#required source/ dest/ path by command line.
+
+if [ $# -lt 2 ]
+then
+	echo "usage: $0 source/ dest/"
+	exit 0
+fi
+
+if [[ ! -d $1 ]] || [[ ! -d $2 ]]
+then
+	echo "bad source/ dest/"
+	exit 0
+fi
+
+for name in $(ls "$1")
+do
+	zip -r "$2/$name.zip" "$1/$name"
+done
+
+exit 1
